dc34df50ed68a9069a6c721030495b1c301a9bf8
heiqie <heiqie@wacai.com>
2018-03-07 20:54:20 +0800

    rebuild

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
 VotingContract = web3.eth.contract(abi);
 // 下面这段代码传入的参数是合约的地址 把在node终端部署智能合约到区块链上时 返回的合约地址作为参数传入
-contractInstance = VotingContract.at('0x7437538612ce7f1b24a19bb3404e0c1d113f734c');
+contractInstance = VotingContract.at('0x474b6b4047440822ce45e5bf1b7620827b9351cd');
 candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"};
 
 function voteForCandidate() {
@@ -18,8 +18,7 @@ $(document).ready(function() {
   console.log(candidateNames)
   for (var i = 0; i < candidateNames.length; i++) {
     let name = candidateNames[i];
-    let val = contract
-    Instance.totalVotesFor.call(name).toString();
+    let val = contractInstance.totalVotesFor.call(name).toString();
     $("#" + candidates[name]).html(val);
   }
 });
